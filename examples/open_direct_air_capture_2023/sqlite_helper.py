abd31440aa060e70cd2994f56d1ef299cdfc1d47
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2025-08-29 18:21:15 -0400

    added debugging functions to 1) get all rows by data type / path id, and 2) remove all rows by data type

--- examples/open_direct_air_capture_2023/sqlite_helper.py
+++ examples/open_direct_air_capture_2023/sqlite_helper.py
@@ -31,6 +31,20 @@ class DB:
         filenames = [name[0] for name in names_tuples]
         return filenames
 
+    def get_all(self, data_type):
+        self.cur.execute("select payload from graph_batches where path_id like ?", (f"{data_type}%",))
+        rows = self.cur.fetchall()
+        blobs = [row[0] for row in rows]
+        return blobs
+
+    def remove_all(self, data_type):
+        # Remove all rows where path_id starts with data_type
+
+        self.cur.execute("DELETE from graph_batches where path_id like ?", (f"{data_type}%",))
+        self.conn.commit()
+
+
     def __del__(self):
         self.conn.commit()
         self.conn.close()
+
