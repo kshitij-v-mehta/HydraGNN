ff7ea3ea7bdcc047ad8b319af63e7f09b32c3c45
Kshitij V. Mehta <kshitij-v-mehta@github.com>
2025-09-19 14:00:24 -0400

    Previous bugfix fixes performance issue. Now reading adios, applying graph transforms, and writing back adios in the same script.

--- utilities/graph_gps/graphgps_transform.py
+++ utilities/graph_gps/graphgps_transform.py
@@ -6,16 +6,43 @@ import sys
 import time
 
 import torch
+from fontTools.misc.psCharStrings import t2Operators
 from mpi4py import MPI
 from torch_geometric.transforms import AddLaplacianEigenvectorPE
 
+from hydragnn.utils.datasets import AdiosDataset, AdiosWriter
 from hydragnn.utils.descriptors_and_embeddings.chemicaldescriptors import (
     ChemicalFeatureEncoder,
 )
 from hydragnn.utils.descriptors_and_embeddings.topologicaldescriptors import (
     compute_topo_features,
 )
-from utilities.graph_gps.db import DB
+from hydragnn.utils.distributed import nsplit
+
+
+def read_adios_data(adios_in, rank, nproc, comm=MPI.COMM_WORLD):
+    common_variable_names = ["x", "edge_index", "edge_attr", "energy", "energy_per_atom", "forces", "pos", "y"]
+
+    trainset = AdiosDataset(adios_in, "trainset", comm)
+    valset = AdiosDataset(adios_in, "valset", comm)
+    testset = AdiosDataset(adios_in, "testset", comm)
+
+    for dataset in (trainset, valset, testset):
+        rx = list(nsplit(range(len(dataset)), nproc))[rank]
+        dataset.setkeys(common_variable_names)
+        dataset.setsubset(rx[0], rx[-1] + 1, preload=True)
+
+    return trainset, valset, testset
+
+
+def write_adios_data(adios_out, trainset, valset, testset, comm):
+    adwriter = AdiosWriter(adios_out, comm)
+    adwriter.add("trainset", trainset)
+    adwriter.add("valset", valset)
+    adwriter.add("testset", testset)
+    adwriter.add_global("pna_deg", trainset.pna_deg)
+    adwriter.add_global("dataset_name", trainset.dataset_name)
+    adwriter.save()
 
 
 def prepare_transform():
@@ -57,46 +84,47 @@ if __name__ == "__main__":
     rank = comm.Get_rank()
     size = comm.Get_size()
 
-    assert len(sys.argv) == 4, f"Run as {sys.argv[0]} <name of dataset> <path to config file> <path to db dir>"
-    dataset_name = sys.argv[1]
-    configfile = sys.argv[2]
-    db_dir_path = sys.argv[3]
+    assert len(sys.argv) == 4, f"Run as {sys.argv[0]} <input adios file> <output adios file> <config file>"
+    adios_in = sys.argv[1]
+    adios_out = sys.argv[2]
+    configfile = sys.argv[3]
 
-    # Read dataset config file
     with open(configfile, "r") as f:
         config = json.load(f)
 
-    # Open connection to db
-    if rank == 0:
-        dbfiles = glob.glob(os.path.join(db_dir_path, "*.db"))
-        assert len(dbfiles) == size, f"Run this with {len(dbfiles)} MPI ranks."
-
-    mydbfile = os.path.join(db_dir_path, dataset_name + f"_{rank}.db")
-    db = DB(mydbfile)
-
     ChemEncoder, lpe_transform = prepare_transform()
 
-    # Process all pyg objects
-    if rank == 0: t1 = time.time()
-    while True:
-        row_data = db.get_unprocessed()
-        if row_data is None:
-            break
+    if rank == 0:
+        print("Reading adios data")
+        t1 = time.time()
 
-        rowid, set_type, pyg_blob, _pyg_transformed = row_data
-        pyg = pickle.loads(pyg_blob)
-        print(f"Rank {rank} applying transform on row id {rowid}")
-        gpst1 = time.time()
-        pyg_transformed = graphgps_transform(ChemEncoder, lpe_transform, pyg, config)
-        gpst2 = time.time()
-        pyg_transformed_blob = pickle.dumps(pyg_transformed)
-        iot1 = time.time()
-        db.update_pyg_transformed(rowid, pyg_transformed_blob)
-        iot2 = time.time()
-        print(f"Rank {rank} updated row id {rowid}. transform time: {round(gpst2-gpst1)}, update time: {round(iot2-iot1)}")
+    trainset, valset, test = read_adios_data(adios_in, rank, size, comm)
 
-    if rank == 0: print(f"Finished transforming {dataset_name} in {round(time.time()-t1)} seconds.")
-    db.close()
+    if rank == 0:
+        t2 = time.time()
+        print(f"Read adios data in {round(t2 - t1)} seconds")
+
+    if rank == 0:
+        print(f"Applying graph GPS transform")
+        t1 = time.time()
+
+    for dataset in (trainset, valset, test):
+        for pyg in dataset:
+            graphgps_transform(ChemEncoder, lpe_transform, pyg, config)
+
+    if rank == 0:
+        t2 = time.time()
+        print(f"Done with graph GPS transform in {round(t2 - t1)} seconds")
+
+    if rank == 0:
+        print("Writing adios data")
+        t1 = time.time()
+
+    write_adios_data(adios_out, trainset, valset, test, comm)
+
+    if rank == 0:
+        t2 = time.time()
+        print(f"Write adios data in {round(t2 - t1)} seconds")
 
     MPI.COMM_WORLD.Barrier()
     if rank == 0: print("All done. Goodbye.")
