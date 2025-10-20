40879c720627dc4061595ba5d08bc1d60c5f5729
Kshitij V. Mehta <kshitij-v-mehta@github.com>
2025-10-20 14:40:54 -0400

    Implemented streaming in of adios pyg objects.

--- utilities/graph_gps/adios_io.py
+++ utilities/graph_gps/adios_io.py
@@ -6,42 +6,43 @@ from adios2 import FileReader
 from hydragnn.utils.distributed import nsplit
 
 
-def read_adios_data(adios_in):
+chunksize = 10
+
+
+def read_adios_dataset(adios_in, label):
+    global chunksize
     comm = mpi_utils.node_roots_comm
     nproc = mpi_utils.node_roots_size
     rank = mpi_utils.node_roots_rank
 
-    trainset = AdiosDataset(adios_in, "trainset", comm)
-    valset = AdiosDataset(adios_in, "valset", comm)
-    testset = AdiosDataset(adios_in, "testset", comm)
+    dataset = AdiosDataset(adios_in, label, comm)
+    rx = list(nsplit(range(len(dataset)), nproc))[rank]
+    start = rx[0]
+    while start < rx[-1] + 1:
+        end = min(start + chunksize, rx[-1] + 1)
+        dataset.setsubset(start, end, preload=True)
+        pyg_chunk = [pyg for pyg in dataset]
+        yield pyg_chunk
 
-    datasets = {trainset.label: [], valset.label: [], testset.label: [], 'extra_attrs': None}
+        start += chunksize
 
-    for dataset in (trainset, valset, testset):
-        rx = list(nsplit(range(len(dataset)), nproc))[rank]
-        print(f"Rank {rank} on {mpi_utils.hostname} reading indices {rx[0]} to {rx[-1]} of {len(dataset)}")
-        dataset.setsubset(rx[0], rx[-1] + 1, preload=True)
 
-        # Iterate to populate the PyG Data object
-        pyg_l = list()
-        for pyg in dataset:
-            pyg_l.append(pyg)
-        datasets[dataset.label] = pyg_l
+def read_extra_attrs(adios_in):
+    comm = mpi_utils.node_roots_comm
+    rank = mpi_utils.node_roots_rank
 
-    # Copy any additional attributes in the ADIOS file
-    write_attrs = dict()
+    extra_attrs = dict()
     if rank == 0:
         with FileReader(adios_in) as f:
             attr = f.available_attributes()
             for a in attr.keys():
                 if not any(
-                    s in a for s in ["trainset/", "valset/", "testset/", "total_ndata"]
+                        s in a for s in ["trainset/", "valset/", "testset/", "total_ndata"]
                 ):
-                    write_attrs[a] = f.read_attribute(a)
+                    extra_attrs[a] = f.read_attribute(a)
 
-    write_attrs = comm.bcast(write_attrs, root=0)
-    datasets['extra_attrs'] = write_attrs
-    return datasets
+    extra_attrs = comm.bcast(extra_attrs, root=0)
+    return extra_attrs
 
 
 def write_adios_data(adios_out, datasets):
--- utilities/graph_gps/node_root.py
+++ utilities/graph_gps/node_root.py
@@ -1,56 +1,50 @@
-from adios_io import read_adios_data, write_adios_data
+from adios_io import read_adios_dataset, write_adios_data
 import mpi_utils
 import traceback
 from logger import logger
 from mpi4py import MPI
 import time
 
+from utilities.graph_gps.adios_io import read_extra_attrs
+
 
 def node_root(adios_in, adios_out):
     try:
-        # Read adios data
-        task_size = 10000
-        t1 = time.time()
-        datasets_in = read_adios_data(adios_in)
-        t2 = time.time()
-        if mpi_utils.node_roots_rank == 0: logger.info(f"ADIOS reading done in {round(t2-t1)} seconds.")
-
         # Hold transformed pyg objects in a new dict
-        datasets_out = dict()
-        for k in datasets_in.keys():
-            if k == 'extra_attrs':
-                datasets_out[k] = datasets_in[k]
-                continue
-            datasets_out[k] = []
+        datasets_out = {'trainset': [], 'valset': [], 'testset': []}
 
-        # iterate over trainset, valset, and testset and send pyg objects to workers
         t1 = time.time()
-        logger.debug(f"Node root on {mpi_utils.hostname} sending tasks to workers")
-        status = MPI.Status()
-        workers_free = 0
-        for k in datasets_in.keys():
-            if k == 'extra_attrs': continue
+        for label in ('trainset', 'valset', 'testset'):
+            workers_free = 0
+            status = MPI.Status()
 
-            # Split list of PyG objects and send to workers
-            dataset = datasets_in[k]
-            while len(dataset) > 0:
-                # Receive ping or completed tasks from a worker
-                data_t = mpi_utils.node_comm.recv(source=MPI.ANY_SOURCE, status=status)
-                if data_t is not None:
-                    key, transformed_pyg_obj = data_t
-                    datasets_out[key].extend(transformed_pyg_obj)
-                else:
-                    pass  # this was a ready ping from a worker
-                worker = status.Get_source()
-                workers_free += 1
+            # iterate over trainset, valset, and testset and send pyg objects to workers
+            logger.debug(f"Node root on {mpi_utils.hostname} sending tasks in {label} to workers")
+            for pyg_chunk in read_adios_dataset(adios_in, label):
+                task_size = len(pyg_chunk) // (mpi_utils.node_size * 2)
 
-                # Send a chunk of objects to the worker
-                pyg_chunks = []
-                next_task_size = min(task_size, len(dataset))
-                for _ in range(next_task_size):
-                    pyg_chunks.append(dataset.pop())
-                mpi_utils.node_comm.send((k, pyg_chunks), dest=worker)
-                workers_free -= 1
+                # chop into tasks and dynamically assign them to workers
+                while len(pyg_chunk) > 0:
+                    # get ping/data from worker
+                    data_t = mpi_utils.node_comm.recv(source=MPI.ANY_SOURCE, status=status)
+                    if data_t is not None:
+                        key, transformed_pyg_obj = data_t
+                        datasets_out[key].extend(transformed_pyg_obj)
+                    else:
+                        pass  # this was a ready ping from a worker
+                    worker = status.Get_source()
+                    workers_free += 1
+
+                    # send a chunk of tasks to worker
+                    task_chunk = []
+                    for _ in range(task_size):
+                        if len(pyg_chunk) == 0:
+                            break
+                        task_chunk.append(pyg_chunk.pop())
+
+                    if len(task_chunk) > 0:
+                        mpi_utils.node_comm.send((label, task_chunk), dest=worker)
+                        workers_free -= 1
 
         # Wait for all workers to finish
         logger.debug(f"Node root on {mpi_utils.hostname} signaling workers to quit.")
@@ -65,6 +59,7 @@ def node_root(adios_in, adios_out):
         for worker in range(1, mpi_utils.node_size):
             mpi_utils.node_comm.send(None, dest=worker)
 
+        datasets_out['extra_attrs'] = read_extra_attrs(adios_in)
         t2 = time.time()
         logger.info(f"Graph transforms on {mpi_utils.hostname} done in {round(t2-t1)} seconds.")
 
@@ -78,25 +73,3 @@ def node_root(adios_in, adios_out):
     except Exception as e:
         logger.error(f"Exception {e} at {traceback.format_exc()}")
         MPI.COMM_WORLD.Abort(1)
-
-
-def _split_pyg_list(pyg_l, n):
-    """
-    Splits a list of PyG objects into n lists
-    """
-    sublists = list()
-    listsize = len(pyg_l)
-    k, m = divmod(listsize, n)
-
-    for _ in range(n):
-        l = []
-        for i in range(k):
-            obj = pyg_l.pop()
-            l.append(obj)
-        sublists.append(l)
-
-    for i, _ in enumerate(range(m)):
-        obj = pyg_l.pop()
-        sublists[i].append(obj)
-
-    return sublists
